ca131648c1b5a1399a7962797ad8a707342fba8c
Lars Reuter <lp.reuter@gmail.com>
2024-01-14 19:20:15 +0100

    Init simple server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import http from 'node:http'; // Import HTTP build-in module for creating web servers
+import fs from 'node:fs';
+import http from 'node:http';
+import { URL } from 'node:url';
 
-http.createServer();
+const port = 8080;
+const directory = '/public';
+const requestListener = (req, res) => {
+  res.end('Server running...');
+};
+const server = http.createServer(requestListener);
+
+server.listen(port);
